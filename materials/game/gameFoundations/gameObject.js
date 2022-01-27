5944e4e87c7656a2932bc23ae195a37f5109c150
Carson Burke <carsonburke22@gmail.com>
2022-01-27 11:48:26 -0800

    Removed path text

--- materials/game/gameFoundations/gameObject.js
+++ materials/game/gameFoundations/gameObject.js
@@ -51,7 +51,7 @@ GameObject.prototype.moveTo = function(targetPos) {
     function generatePath() {
 
         // Construct starts and ends based on the gameObject pos and target left and top
-    
+
         const start = game.graph.grid[gameObject.pos.left / gridSize][gameObject.pos.top / gridSize]
         const end = game.graph.grid[targetPos.left / gridSize][targetPos.top / gridSize]
 
@@ -66,36 +66,27 @@ GameObject.prototype.moveTo = function(targetPos) {
         }
 
         // Generate a path from start to end using the graph
-        
+
         gameObject.path = astar.search(game.graph, start, end)
     }
 
     // Stop if there is no path
-    
+
     if (gameObject.path.length == 0) return false
-
-    // Loop through positions in the path
-
-    for (const pos of gameObject.path) {
-
-        // Visualize the text's weight
-
-        game.cm.fillText(pos.weight, pos.x * gridSize + gridSize / 2, pos.y * gridSize + gridSize / 2);
-    }
 
     // Construct the first pos
 
     const firstPos = gameObject.path[0]
-        
+
     // Move to the first pos in the path and inform true
-    
+
     gameObject.pos.left = firstPos.x * gridSize
     gameObject.pos.top = firstPos.y * gridSize
 
     // Remove the first element of the path
 
     gameObject.path.shift()
-    
+
     // And inform true
 
     return true
@@ -139,9 +130,9 @@ GameObject.prototype.breed = function(inputs, outputs) {
         child.network = gameObject.network.clone(inputs, outputs)
 
         child.network.learn()
-        
+
         gameObject.childAmount++
-        newChildAmount++
+            newChildAmount++
     }
 }
 
@@ -150,7 +141,7 @@ GameObject.prototype.findHuntTarget = function(type) {
     const gameObject = this
 
     const creaturesOfType = Object.values(game.objects[type])
-    
+
     const closestCreatureOfType = gameObject.pos.sortGameObjectsByDistance(creaturesOfType)[0]
     return closestCreatureOfType
 }
@@ -164,7 +155,7 @@ GameObject.prototype.hunt = function(type) {
 
     if (gameObject.pos.getDistance(huntTarget.pos) > gridSize * 1.5) {
 
-        gameObject.moveTo(huntTarget.pos) 
+        gameObject.moveTo(huntTarget.pos)
         return false
     }
 
@@ -202,7 +193,7 @@ GameObject.prototype.drink = function() {
     }
 
     if (gameObject.resources.water < resourceCarryCapacity) gameObject.resources.water++
-    return true
+        return true
 }
 
 GameObject.prototype.forage = function() {
@@ -217,7 +208,7 @@ GameObject.prototype.forage = function() {
 
     if (gameObject.pos.getDistance(closestBush.pos) > gridSize * 1.5) {
 
-        gameObject.moveTo(closestBush.pos) 
+        gameObject.moveTo(closestBush.pos)
         return false
     }
 
@@ -231,7 +222,7 @@ GameObject.prototype.updateStats = function() {
 
     gameObject.age++
 
-    gameObject.health -= Math.random() * gameObject.age / 100000
+        gameObject.health -= Math.random() * gameObject.age / 100000
 
     if (gameObject.resources.food <= 0) gameObject.health -= 0.1
     if (gameObject.resources.water <= 0) gameObject.health -= 0.1
@@ -243,51 +234,51 @@ GameObject.prototype.updateStats = function() {
 }
 
 GameObject.prototype.createNetwork = function(inputs, outputs) {
-    
+
     const gameObject = this
 
     // Create neural network
-    
+
     const network = new NeuralNetwork()
-    
+
     // Create layers
-    
+
     let layerCount = 2
-    
+
     for (let i = 0; i < layerCount; i++) network.addLayer()
-    
+
     // Create perceptrons
-    
+
     // Create input perceptrons
-    
+
     for (let i = 0; i < inputs.length; i++) network.layers[0].addPerceptron()
-    
+
     // Create hidden perceptrons
-    
+
     let hiddenPerceptronsNeed = 4
-    
+
     // Loop through layers
-    
+
     for (let layerName in network.layers) {
-    
+
         // Filter only hidden layers
-    
+
         let layersCount = Object.keys(network.layers).length
-    
+
         if (layerName > 0 && layerName < layersCount - 1) {
-    
+
             let layer = network.layers[layerName]
-    
+
             for (let i = 0; i < hiddenPerceptronsNeed; i++) layer.addPerceptron()
         }
     }
-    
+
     // Create output perceptrons
-    
+
     for (let i = 0; i < outputs.length; i++) network.layers[layerCount - 1].addPerceptron()
-    
+
     //
-    
+
     network.init(inputs, outputs)
 
     gameObject.network = network
